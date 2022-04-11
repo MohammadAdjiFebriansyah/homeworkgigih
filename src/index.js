b12aa68a283b617c306addf9fc933b1d5f1b27eb
MohammadAdjiFebriansyah <adji280201@gmail.com>
2022-04-11 10:36:43 +0700

    homework modul 4 session 3

--- src/index.js
+++ src/index.js
@@ -7,11 +7,14 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import App from './App';
 import { Provider } from 'react-redux';
 import store from './store';
+import {BrowserRouter as Router} from 'react-router-dom';
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-       <App />
+    <Router>
+        <App />
+      </Router>
        <ToastContainer />
      </Provider>
   </React.StrictMode>,
